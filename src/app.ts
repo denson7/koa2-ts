82044f2e8861448064a72942b19237979f5123e0
denson7 <2671475697@qq.com>
2020-05-25 14:38:59 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,14 @@ import jsonError from 'koa-json-error';
 import cors from 'koa2-cors';
 import koaHelmet from 'koa-helmet';
 import path from 'path';
+import session from 'koa-session';
 import router from './routes';
+import errTrace from './middleware/trace';
+const parameter = require('koa-parameter');
+
 // 创建实例
 const app = new Koa();
-const parameter = require('koa-parameter');
+app.use(errTrace);
 
 // 注册错误处理中间件
 app.use(
@@ -17,7 +21,19 @@ app.use(
     postFormat: (e, { stack, ...rest }) => (process.env.NODE_ENV === 'production' ? { stack, ...rest } : rest)
   })
 );
-
+app.keys = ['jwt secret'];
+const session_config = {
+  key: 'koa:sess', // cookie key (default is koa:sess)
+  // maxAge: 24 * 60 * 60 * 1000, //  1 days cookie expire time, (millisecond)
+  maxAge: 60 * 60 * 1000,
+  autoCommit: true, // automatically commit headers (default true)
+  overwrite: true, // overwrite (default true)
+  httpOnly: true, // httpOnly (default true)
+  signed: true, // signed (default true)
+  rolling: true, // Force a session identifier cookie to be set on every response (default false)
+  renew: false // renew session when session is nearly expired (default false)
+};
+app.use(session(session_config, app));
 app.use(
   koaBody({
     multipart: true, // 支持文件上传
@@ -42,6 +58,9 @@ app.use(koaHelmet());
 // 注册路由
 // app.use(koaRouter);
 router(app);
+app.on('error', (err, ctx) => {
+  logger.error('server error', err);
+});
 
 // 启动服务
 app.listen(config.get('port'), () => {
